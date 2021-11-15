5d102bc43391d43266f5f87cc7c05debd2299f12
Ayoub F <94278725+ayslaw@users.noreply.github.com>
2021-11-15 04:33:01 -0500

    Update chorus-for.js

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,14 @@ for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
+
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
